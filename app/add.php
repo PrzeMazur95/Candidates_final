33ed44d955481b27242521b8588a3956005b287d
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-06-16 14:58:46 +0200

    Add Candidates form page, add page which process request to add a candidate, create necesary methods in CandidateController, and CandidateModel to add new candidate to DB, add 3 tests

---
+++ app/add.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+require_once("autoloader.php");
+
+
+if(isset($_POST['submit'])){
+
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $age = $_POST['age'];
+    $role = $_POST['role'];
+    $date = $_POST['date'];
+
+    $candidate = new CandidateController();
+
+    if($candidate->emptyInput($first_name, $last_name, $age, $date, $role)){
+
+        if(is_int($age) || $age>0){
+
+            $candidate->addCandidate($first_name, $last_name, $age, $date, $role);
+
+        } else {
+
+        $_SESSION['msg']="Age cannot be less than 0, or you did not type a number!";
+        $_SESSION['msg_type']="danger";
+        header("location: addCandidateForm.php");
+
+        }
+
+    }else{
+
+        $_SESSION['msg']="You have to type all fields!";
+        $_SESSION['msg_type']="danger";
+        header("location: addCandidateForm.php");
+
+    }
+
+}else{
+
+    $_SESSION['msg']="You have to submit the form";
+    $_SESSION['msg_type']="danger";
+
+    header("location: addCandidateForm.php");
+
+}
+
+?>
